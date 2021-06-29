0327e41cfc9455ad82183f74bc79b044c0c353e3
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-06-29 11:25:39 +0900

    [Add] ログイン画面のレイアウト

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,29 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const Login: VFC = memo(() => {
-  return <p>ログインページ</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg">
+          ユーザー管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="ユーザーID" />
+          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+            ログイン
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
